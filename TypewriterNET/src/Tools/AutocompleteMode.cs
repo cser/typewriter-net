182b6faafc536738f65d2ff8ba35565c5badc71f
sergei.chelnokov <diehacke@yandex.ru>
2016-10-03 14:12:51 +0300

    More correct autocomplete list sorting

--- TypewriterNET/src/Tools/AutocompleteMode.cs
+++ TypewriterNET/src/Tools/AutocompleteMode.cs
@@ -59,7 +59,8 @@ public class AutocompleteMode
 	
 	private void UpdateItems()
 	{
-		string word = textBox.Controller.Lines.GetText(startCaret, caret - startCaret).ToLower();
+		string word = textBox.Controller.Lines.GetText(startCaret, caret - startCaret);
+		string wordIgnoredCase = word.ToLower();
 		List<ToolStripItem> items = new List<ToolStripItem>();
 		filteredVariants.Clear();
 		selectedVariant = null;
@@ -67,14 +68,42 @@ public class AutocompleteMode
 		{
 			Variant variant = variants[i];
 			if (variant.CompletionText == null || variant.DisplayText == null ||
-				!string.IsNullOrEmpty(word) && !variant.CompletionText.ToLower().Contains(word))
+				!string.IsNullOrEmpty(word) && !variant.CompletionText.ToLower().Contains(wordIgnoredCase))
 				continue;
-			if (selectedVariant == null)
-				selectedVariant = variant;
 			filteredVariants.Add(variant);
 		}
+		Compare_Word = word;
+		Compare_WordIgnoredCase = wordIgnoredCase;
+		filteredVariants.Sort(CompareFilteredVariants);
+		if (selectedVariant == null && filteredVariants.Count > 0)
+			selectedVariant = filteredVariants[0];
 		dropDown.SetVariants(filteredVariants);
 		dropDown.SetSelectedVariant(selectedVariant);
+	}
+	
+	private string Compare_Word;
+	private string Compare_WordIgnoredCase;
+	
+	private int CompareFilteredVariants(Variant v0, Variant v1)
+	{
+	    if (string.IsNullOrEmpty(Compare_Word))
+	        return string.Compare(v0.CompletionText, v1.CompletionText);
+	    string text0 = v0.CompletionText.ToLower();
+	    string text1 = v1.CompletionText.ToLower();
+	    int index0 = text0.IndexOf(Compare_WordIgnoredCase);
+	    int index1 = text1.IndexOf(Compare_WordIgnoredCase);
+	    if (index0 == -1 || index1 == -1)
+	        return index1 - index0;
+	    if (index0 == index1)
+	    {
+	        index0 = v0.CompletionText.IndexOf(Compare_Word);
+            index1 = v1.CompletionText.IndexOf(Compare_Word);
+            if (index0 == -1 || index1 == -1)
+	            return index1 - index0;
+	        if (index0 == index1)
+	            string.Compare(v0.CompletionText, v1.CompletionText);
+	    }
+	    return index0 - index1;
 	}
 	
 	public void Close()
@@ -91,7 +120,10 @@ public class AutocompleteMode
 	private bool DoMoveUp(Controller controller)
 	{
 		if (dropDown == null || filteredVariants.Count == 0)
+		{
+			Close();
 			return true;
+		}
 		if (selectedVariant == null || !filteredVariants.Contains(selectedVariant))
 			selectedVariant = filteredVariants[filteredVariants.Count - 1];
 		int index = filteredVariants.IndexOf(selectedVariant);
@@ -103,7 +135,10 @@ public class AutocompleteMode
 	private bool DoMoveDown(Controller controller)
 	{
 		if (dropDown == null || filteredVariants.Count == 0)
-			return true;
+		{
+			Close();
+			return false;
+		}
 		if (selectedVariant == null || !filteredVariants.Contains(selectedVariant))
 			selectedVariant = filteredVariants[0];
 		int index = filteredVariants.IndexOf(selectedVariant);
@@ -127,7 +162,7 @@ public class AutocompleteMode
 			if (count < 0 || count > completionText.Length)
 			{
 				Close();
-				return true;
+				return false;
 			}
 			textBox.Controller.InsertText(completionText.Substring(count));
 		}
