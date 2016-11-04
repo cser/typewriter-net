c95502e1ef0ff5f87e495448867c983bd4bf2722
technologer <csernt@yandex.ru>
2016-11-05 00:33:20 +0300

    fail on remove reproduced

--- MulticaretEditorTests/src/FailOnTextEditTest.cs
+++ MulticaretEditorTests/src/FailOnTextEditTest.cs
@@ -20,7 +20,7 @@ namespace UnitTests
 		}
 		
 		[Test]
-		public void MastNotFail()
+		public void MastNotFail_1()
 		{
 			LineArray lines = new LineArray(100);
 			Controller controller = new Controller(lines);
@@ -31,6 +31,18 @@ namespace UnitTests
 			controller.PutCursor(new Place(0, 305), false);
 			controller.Paste();
 			Assert.AreEqual(File.ReadAllText("../test_code_after.txt"), lines.GetText());
+		}
+		
+		[Test]
+		public void MastNotFail_2()
+		{
+			LineArray lines = new LineArray(10);
+			Controller controller = new Controller(lines);
+			lines.SetText(File.ReadAllText("../test_code_2.txt"));
+			controller.PutCursor(new Place(0, 9), false);
+			controller.PutCursor(new Place(0, 20), true);
+			controller.Cut();
+			Assert.AreEqual(File.ReadAllText("../test_code_2_after.txt"), lines.GetText());
 		}
 	}
 }
