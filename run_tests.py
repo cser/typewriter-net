2d1dd1851531468a6121bc3e550c11724008f341
technologer <csernt@yandex.ru>
2016-11-06 02:03:52 +0300

    more clear test output

--- run_tests.py
+++ run_tests.py
@@ -5,6 +5,8 @@ proc = subprocess.Popen(["c:\\Windows\\Microsoft.NET\\Framework\\v2.0.50727\\MSB
 out = proc.stdout.read()
 if "<<<" in out:
 	out = out[out.index("<<<"):]
+elif "ProcessModel: Default    DomainUsage: Multiple" in out:
+	out = out[out.index("ProcessModel: Default    DomainUsage: Multiple"):]
 out = out.replace('\r\n', '\n')
 for line in out.split('\n'):
 	if line.startswith('    '):
