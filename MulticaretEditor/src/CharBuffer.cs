20d8fb9d8d069258a61056f4c9f097f3943386c7
expl <csernt@yandex.ru>
2017-03-22 23:27:44 +0300

    simplify buffer

--- MulticaretEditor/src/CharBuffer.cs
+++ MulticaretEditor/src/CharBuffer.cs
@@ -11,44 +11,39 @@ namespace MulticaretEditor
         
         public CharBuffer()
 		{
-			buffer = new char[MinCapacity];
 		}
         
         public void Resize(int count)
 		{
-			if (count > buffer.Length)
+			int length = buffer != null ? buffer.Length : MinCapacity;
+			if (count > length)
 			{
-				int nextLength = buffer.Length << 1;
+				int nextLength = length << 1;
 				while (nextLength < count)
 				{
 					nextLength = nextLength << 1;
 				}
-				char[] newBuffer = new char[nextLength];
-				Array.Copy(buffer, newBuffer, this.count);
-				buffer = newBuffer;
+				buffer = new char[nextLength];
+				length = nextLength;
 			}
-			else if (count < this.count)
+			else if (buffer == null)
 			{
-				Array.Clear(buffer, count, this.count - count);
+				buffer = new char[MinCapacity];
 			}
-			this.count = count;
-		}
-		
-		public void Realocate()
-		{
-			int length = buffer.Length;
-			while (true)
+			else
 			{
-				int halfLength = length >> 1;
-				if (halfLength < MinCapacity || count > (halfLength >> 1))
-					break;
-				length = halfLength;
-			}
-			if (length < buffer.Length)
-			{
-				char[] newBuffer = new char[length];
-				Array.Copy(buffer, newBuffer, newBuffer.Length);
-				buffer = newBuffer;
+				this.count = count;
+				while (true)
+				{
+					int halfLength = length >> 1;
+					if (halfLength < MinCapacity || count > (halfLength >> 1))
+						break;
+					length = halfLength;
+				}
+				if (length < buffer.Length)
+				{
+					buffer = new char[length];
+				}
 			}
 		}
     }
